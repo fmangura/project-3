6154b7d00b83faf441b30f27d2aa7c595a364a15
Franz Manguray <franznmanguray@gmail.com>
2024-08-29 03:03:27 -0700

    edit photo links

--- src/helper/Stack.jsx
+++ src/helper/Stack.jsx
@@ -4,7 +4,7 @@ export default function Stack({codeStack}){
     return (
         <div className='icons-list'>
             {codeStack.map((stack, idx) => 
-                <img key={`${stack}-${idx}`} src={`/Coding/icon-${stack}.png`} className='lang-icons'></img>
+                <img key={`${stack}-${idx}`} src={`Coding/icon-${stack}.png`} className='lang-icons'></img>
             )}
         </div>
     )
