fdcbda1d3c3c24369b34b0f6f12f144ee0be4d54
Franz Manguray <franznmanguray@gmail.com>
2024-08-29 03:11:45 -0700

    fixed logo links

--- src/assets/ListofProjects.jsx
+++ src/assets/ListofProjects.jsx
@@ -5,7 +5,7 @@ export default function ListofProjects(){
     const allProjects = [
         {
             'name': 'Appose',
-            'img': './public/Appose.svg',
+            'img': 'public/Appose.svg',
             'demolink': 'https://posse.onrender.com/',
             'gitlink': 'https://github.com/fmangura/Appose',
             'summary': 'A blog post webapp that analyses a users sentiment towards their discussed topics and allows other users to interact through a poll range of how much they agree with the post. It then provides users with analytics on that post and how the average user feels about that post. Users can also see how might other users interact with other posts.',
@@ -14,7 +14,7 @@ export default function ListofProjects(){
         },
         {
             'name': 'Kitchen Finds',
-            'img': '../public/KitchenFindsPics/KitchenFindsLogo.svg',
+            'img': 'public/KitchenFindsPics/KitchenFindsLogo.svg',
             'demolink': 'https://kitchenfinds.onrender.com/',
             'gitlink': 'https://github.com/fmangura/KitchenFinds',
             'summary': 'A find by ingredients recipe app provided by Spoonful API. Simply add ingredients or pick from suggested list from your query and a list of recipes are presented to you! Choose from what you like to see the instructions or add to your list of ingredients!',
